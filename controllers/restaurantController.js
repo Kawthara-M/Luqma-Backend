993c70604e5e3b94ded9a4fc880083612b0a3ca4
Kawthara <kawtharr.m.ahmad@gmail.com>
2025-08-10 12:13:21 +0300

    cleaning code

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -4,11 +4,9 @@ const GetAllRestaurants = async (req, res) => {
   try {
     const restaurants = await Restaurant.find()
     if (restaurants) {
-      console.log("restaurants: " + restaurants)
       res.send(restaurants)
     } else {
       res.send("no restaurants found")
-      console.log("no restaurants")
     }
   } catch (error) {
     console.log(error)
