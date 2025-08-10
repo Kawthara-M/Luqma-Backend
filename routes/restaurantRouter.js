8b6aa4c2be057af6d6972baad911762d63b480a6
fatemaAlbasri <falbasri4@gmail.com>
2025-08-10 20:57:19 +0300

    adding filterCategoty function and category routes

--- routes/restaurantRouter.js
+++ routes/restaurantRouter.js
@@ -1,8 +1,10 @@
-const router = require("express").Router()
-const restaurantCtrl = require("../controllers/restaurantController")
+const router = require('express').Router()
+const restaurantCtrl = require('../controllers/restaurantController')
 
-router.get("/", restaurantCtrl.GetAllRestaurants)
-router.get("/:id", restaurantCtrl.GetRestaurant)
-router.get("/:id/menu", restaurantCtrl.GetMenu)
+router.get('/', restaurantCtrl.GetAllRestaurants)
+router.get('/:id', restaurantCtrl.GetRestaurant)
+router.get('/:id/menu', restaurantCtrl.GetMenu)
+
+router.get('/cuisine/:cuisineType', restaurantCtrl.GetRestaurantsByCategory)
 
 module.exports = router
