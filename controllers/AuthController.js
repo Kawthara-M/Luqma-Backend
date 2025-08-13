d74fb020607f14a4922a6ebfe3a17e23deef59e7
Kawthara <kawtharr.m.ahmad@gmail.com>
2025-08-13 19:52:38 +0300

    customizing error messages for front end

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -5,39 +5,42 @@ const validatePassword = require("../validators/passwordValidator.js")
 
 const SignUp = async (req, res) => {
   try {
-    const { name, email, phone, password } = req.body
-
-    if (!validatePassword(password)) {
-      return res
-        .status(400)
-        .json({
-          error:
-            "Weak Password! Have a mix of capital and lower letters, digits, and unique symbols!",
-        })
-    }
-
-    let hashPassword = await middleware.hashPassword(password)
-
+    const { name, email, phone, password, confirmPassword } = req.body
     let existingUser = await Customer.findOne({ email })
+
     if (existingUser) {
       return res
-        .status(400)
+        .status(409)
         .json({ error: "A user with that email has already been registered!" })
     } else {
-      const customer = await Customer.create({
-        name,
-        email,
-        phone,
-        passwordDigest: hashPassword,
-      })
-      let payload = {
-        id: customer._id,
-        email: customer.email,
+      if (!validatePassword(password)) {
+        return res.status(400).json({
+          error:
+            "Weak Password! Have a mix of -lower & upper- case letters, digits, and unique symbols!",
+        })
+      } else {
+        if (password === confirmPassword) {
+          let hashPassword = await middleware.hashPassword(password)
+          const customer = await Customer.create({
+            name,
+            email,
+            phone,
+            passwordDigest: hashPassword,
+          })
+          let payload = {
+            id: customer._id,
+            email: customer.email,
+          }
+
+          let token = middleware.createToken(payload)
+
+          return res.send({ user: payload, token })
+        } else {
+          return res.status(400).json({
+            error: "Passwords must match!",
+          })
+        }
       }
-
-      let token = middleware.createToken(payload)
-
-      return res.send({ customer: payload, token })
     }
   } catch (error) {
     throw error
@@ -47,39 +50,31 @@ const SignUp = async (req, res) => {
 const SignIn = async (req, res) => {
   try {
     const { email, passwordDigest } = req.body
-    if (!validatePassword(passwordDigest)) {
-      return res
-        .status(400)
-        .json({
-          error:
-            "Weak Password! Have a mix of capital and lower letters, digits, and unique symbols!",
-        })
-    }
 
     const customer = await Customer.findOne({ email })
 
-    let matched = await middleware.comparePassword(
-      passwordDigest,
-      customer.passwordDigest
-    )
-
-    if (matched) {
-      let payload = {
-        id: customer._id,
-        email: customer.email,
+    if (customer) {
+      let matched = await middleware.comparePassword(
+        passwordDigest,
+        customer.passwordDigest
+      )
+      if (matched) {
+        let payload = {
+          id: customer._id,
+          email: customer.email,
+        }
+        let token = middleware.createToken(payload)
+        return res.send({ user: payload, token })
       }
-      let token = middleware.createToken(payload)
-      return res.send({ user: payload, token })
+    } else {
+      res.status(401).send({ status: "Error", msg: "Invalid Credentials" })
     }
-
-    res.status(401).send({ status: "Error", msg: "Unauthorized" })
   } catch (error) {
     console.log(error)
-    res.status(401).send({ status: "Error", msg: "An error has occurred!" })
+    res.status(401).send({ status: "Error", msg: "Invalid Credentials!" })
   }
 }
 
-// this controller require handling like signout in front-end to clear localStorage
 const deletAccount = async (req, res) => {
   try {
     const userId = req.params.id
